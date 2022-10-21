9f814cf33714a9628ca06bf3f3cb58cf753d049e
AyushShende <ayushshende9@gmail.com>
2022-10-21 17:31:20 +0530

    built footer

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ import {
   PropertyList,
   FeaturedProperties,
   MailList,
+  Footer,
 } from "../../components";
 const Home = () => {
   return (
@@ -19,6 +20,7 @@ const Home = () => {
         <h1 className="homeTitle">Homes guests love</h1>
         <FeaturedProperties />
         <MailList />
+        <Footer />
       </div>
     </div>
   );
