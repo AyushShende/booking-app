8850ed8565156ae4a9aff67a9527f746a36d7a2e
AyushShende <ayushshende9@gmail.com>
2022-10-21 12:59:02 +0530

    built propList component

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,15 @@
 import "./home.css";
-import { Navbar, Header } from "../../components";
+import { Navbar, Header, Featured, PropertyList } from "../../components";
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+      </div>
     </div>
   );
 };
